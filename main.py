87051c39fcfe04b939e5c223063f8b78b881541f
manolo630 <imanol.salazarloran@gmail.com>
2025-10-10 10:42:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from GestorGastos.Gestor_de_gastos import gestor_de_gastos
+
+def menu():
+    print("\n--- Menú Principal ---")
+    print("3.Gestor de gastos")
+    print("5.Salir")
+
+
+def main ():
+  while True:
+    print(menu)
+    opcion=int(input("Ingresa una opcion")
+
+    elif opcion == 3:
+      gestor_de_gastos()
+    elif opcion ==5:
+      break
+    else:
+      print("Ingresa una opcion valida")
+
+main()
 
